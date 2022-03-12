7ec5ce508ece73f62b9186ebc4502ed34eaa6fa2
jeth318 <jesper.thornberg@me.com>
2022-03-12 23:53:21 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+BRANCH_NAME=$1;
+
+if [[ $BRANCH_NAME == "main" ]]; then
+	cd /home/jeth/apps/pc-control
+else
+	exit 0;
+fi
+
+echo "Stashing lock files "
+git stash
+
+echo "Dropping stash"
+git stash drop
+
+echo "Pulling from ${BRANCH_NAME}"
+git pull origin ${BRANCH_NAME}
+echo "Pulled successfully"
+
+
+echo "Installing dependencies"
+npm install
+echo "Project dependencies was installed"
+
+echo "Rebooting pc-control"
+
+if [[ $BRANCH_NAME == "master" ]]; then
+	echo "pm2 restart pc-control"
+else
+    exit 0;
+fi
+echo "Reboot OK"
+echo "Deployment complete, and it was a success!"
+
+exit 0
