a466e45c6276da0c18fdddeed6e78c89f5fcb731
jeth318 <jesper.thornberg@me.com>
2022-04-13 22:36:45 +0200

    Tidy up

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ branch=$(git branch --show-current)
 destination="jeth@10.0.128.110:/home/jeth/apps/pc-control"
 
 if [[ $branch == "main" ]]; then
-    echo "On main branch. Will build and ship to server"
+    echo "On main branch. Will ship to server"
     echo "Shipping dist to server"
     rsync -arvz -e 'ssh -p 2244' --progress $(pwd)/server.py $destination
 else
-    echo "Not on main branch. Skipping build and deploy."
+    echo "Not on main branch. Skipping  deploy."
 fi
